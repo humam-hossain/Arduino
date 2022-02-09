80152c792a81ee0f5cc467e2be830127085d965d
humam-hossain <humam.hossain.e@gmail.com>
2022-02-09 23:15:21 +0600

    updated on Wed Feb  9 11:15:21 PM +06 2022

--- PlatformIOTest/src/main.cpp
+++ PlatformIOTest/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#define onboard 13
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(onboard, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(onboard, LOW);
+  delay(1000);
+  digitalWrite(onboard, LOW);
+  delay(1000);
+
+  Serial.println("loop");
 }
